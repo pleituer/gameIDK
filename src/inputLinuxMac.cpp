2416b48698e8603a00c797caa3ee13a6b1994ce5
pleituer <liuhongzhi3000@gmail.com>
2023-03-18 01:11:52 +0800

    misspelt termios

--- src/inputLinuxMac.cpp
+++ src/inputLinuxMac.cpp
@@ -2,7 +2,7 @@
 #include "inputConstants.h"
 
 #if __APPLE__ || __linux__
-    #include <terminos.h>
+    #include <termios.h>
     #include <unistd.h>
     #include <sys/types.h>
     #include <sys/time.h>
