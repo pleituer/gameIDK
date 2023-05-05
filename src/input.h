6777617985138cfd9feee5a9139761ff5ce2f105
frank719 <115769945+frank719@users.noreply.github.com>
2023-05-05 16:26:34 +0800

    Update input.h

--- src/input.h
+++ src/input.h
@@ -1,9 +1,13 @@
 #pragma once
 
+// Include the header file containing input-related constants
 #include "inputConstants.h"
 
+// Check the platform being used for compilation
 #if _WIN32
+    // If compiling for Windows, include the inputWindows.h header
     #include "inputWindows.h"
 #elif __APPLE__ || __linux__
+    // If compiling for macOS or Linux, include the inputLinuxMac.h header
     #include "inputLinuxMac.h"
 #endif
